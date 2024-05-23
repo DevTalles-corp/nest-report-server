f8b273d4bdc3d4fd39ce615829348dee9a41dd76
Fernando Herrera <fernando.herrera85@gmail.com>
2024-05-23 08:51:59 -0400

    chore: Update package dependencies and add @kurkle/color library

--- src/helpers/chart-utils.ts
+++ src/helpers/chart-utils.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import colorLib from '@kurkle/color';
 
 interface ChartOptions {
   height?: number;
@@ -22,3 +23,109 @@ export const chartJsToImage = async (
 
   return `data:image/png;base64,${Buffer.from(response.data).toString('base64')}`;
 };
+
+let _seed = Date.now();
+
+export function srand(seed: any) {
+  _seed = seed;
+}
+
+export function rand(min: number = 0, max: number = 0) {
+  _seed = (_seed * 9301 + 49297) % 233280;
+  return min + (_seed / 233280) * (max - min);
+}
+
+export const CHART_COLORS = [
+  '#4dc9f6',
+  '#f67019',
+  '#f53794',
+  '#537bc4',
+  '#acc236',
+  '#166a8f',
+  '#00a950',
+  '#727478',
+  '#9756cf',
+];
+
+export const NAMED_COLORS = {
+  red: 'rgb(255, 99, 132)',
+  orange: 'rgb(255, 159, 64)',
+  yellow: 'rgb(255, 205, 86)',
+  green: 'rgb(75, 192, 192)',
+  blue: 'rgb(54, 162, 235)',
+  purple: 'rgb(153, 102, 255)',
+  grey: 'rgb(201, 203, 207)',
+};
+
+export function transparentize(value: string, opacity: number) {
+  const alpha = opacity === undefined ? 0.5 : 1 - opacity;
+  return colorLib(value).alpha(alpha).rgbString();
+}
+
+export interface NumbersConfig {
+  min?: number;
+  max?: number;
+  count?: number;
+  from?: number;
+  decimals?: number;
+  continuity?: number;
+}
+
+export function numbers(config: NumbersConfig = {}) {
+  const cfg = config || {};
+  const min = cfg.min ?? 0;
+  const max = cfg.max ?? 100;
+  const from = cfg.from ?? [];
+  const count = cfg.count ?? 8;
+  const decimals = cfg.decimals ?? 8;
+  const continuity = cfg.continuity ?? 1;
+  const dfactor = Math.pow(10, decimals) || 0;
+  const data = [];
+  let i: number, value: number;
+
+  for (i = 0; i < count; ++i) {
+    value = (from[i] || 0) + rand(min, max);
+    if (rand() <= continuity) {
+      data.push(Math.round(dfactor * value) / dfactor);
+    } else {
+      data.push(null);
+    }
+  }
+
+  return data;
+}
+
+const MONTHS = [
+  'Enero',
+  'Febrero',
+  'Marzo',
+  'Abril',
+  'Mayo',
+  'Junio',
+  'Julio',
+  'Agosto',
+  'Septiembre',
+  'Octubre',
+  'Noviembre',
+  'Diciembre',
+];
+
+interface MonthsConfig {
+  count?: number;
+  section?: number;
+}
+
+export function months(config: MonthsConfig = {}) {
+  const cfg = config ?? {};
+  const count = cfg.count ?? 12;
+  const section = cfg.section;
+  const values = [];
+  let i: number, value: string;
+
+  for (i = 0; i < count; ++i) {
+    value = MONTHS[Math.ceil(i) % 12];
+    values.push(value.substring(0, section));
+  }
+
+  return values;
+}
--- src/reports/basic-chart-svg.report.ts
+++ src/reports/basic-chart-svg.report.ts
@@ -25,9 +25,44 @@ const generateChartImage = async () => {
   return Utils.chartJsToImage(chartConfig);
 };
 
+const generateDonut = async () => {
+  const DATA_COUNT = 5;
+  const NUMBER_CFG = { count: DATA_COUNT, min: 0, max: 100 };
+
+  const data = {
+    labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: Utils.numbers(NUMBER_CFG),
+        backgroundColor: Object.values(Utils.CHART_COLORS),
+      },
+    ],
+  };
+
+  const config = {
+    type: 'doughnut',
+    data: data,
+    options: {
+      title: {
+        display: true,
+        text: 'Chart.js Doughnut Chart',
+      },
+    },
+  };
+
+  return Utils.chartJsToImage(config);
+};
+
 export const getBasicChartSvgReport =
   async (): Promise<TDocumentDefinitions> => {
-    const chart = await generateChartImage();
+    const [chart, chartDonut] = await Promise.all([
+      generateChartImage(),
+      generateDonut(),
+    ]);
+
+    // const chart = await generateChartImage();
+    // const chartDonut = await generateDonut();
 
     return {
       content: [
@@ -39,6 +74,10 @@ export const getBasicChartSvgReport =
         },
         {
           image: chart,
+          width: 500,
+        },
+        {
+          image: chartDonut,
           width: 500,
         },
       ],
